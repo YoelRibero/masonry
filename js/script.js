60d460ae72e06eab4e3bdc056170fb8b7749be1c
Yoel Ribero <yoelribero9@gmail.com>
2019-07-23 22:52:58 -0300

    Repo masonry

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// external js: masonry.pkgd.js, imagesloaded.pkgd.js
+
+// init Masonry
+var grid = document.querySelector('.grid');
+
+var msnry = new Masonry( grid, {
+  itemSelector: '.grid-item',
+  columnWidth: '.grid-sizer',
+  percentPosition: true
+});
